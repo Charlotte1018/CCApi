b4d9026ab8ae382c928bc3ee423496dfe78f0b29
chenlin <chenlin@gepingtech.com>
2017-07-13 08:38:32 +0800

    修复一个bug

--- js/routes/userApi.js
+++ js/routes/userApi.js
@@ -7,7 +7,7 @@ var sqlDBUtils = require('../../js/sqlDBUtils');
 var md5 = require('md5');
 
 
-router.post("/create", function(req, res) {
+router.post("/create", function (req, res) {
     var user = req.body;
 
     var password = user.password;
@@ -20,57 +20,58 @@ router.post("/create", function(req, res) {
     userModel.create(user, (err) => {
         if (err) throw err;
         res.send({
-            "status":0,
+            "status": 0,
             "result": user
         });
     });
 });
 
-router.post("/login",function(req,res){
+router.post("/login", function (req, res) {
     var name = req.body.name;
     var password = req.body.password;
     var md5Psw = md5(password);
 
     var userModel = sqlDBUtils.getModels().user;
-    userModel.find({name: name},1, (err, result) => {
+    userModel.find({ name: name }, 1, (err, result) => {
         if (err) throw err;
         // console.log(result[0].password);
-        if (result[0].password == md5Psw)
-            res.send(true);
+        if (result.length == 0) { res.send(false) }
+        else if (result[0].password == md5Psw)
+        { res.send(true); }
         else res.send(false);
     });
 })
 
-router.post("/update/:id", function(req, res) {
+router.post("/update/:id", function (req, res) {
     var id = req.params.id;
     var newUser = req.body;
     var userModel = sqlDBUtils.getModels().user;
-    userModel.find({id: id}, (err, result) => {
+    userModel.find({ id: id }, (err, result) => {
         if (err) throw err;
         if (!result || !result.length) {
             res.send([]);
             return;
         }
         result = Object.assign(result[0], newUser);
-        result.save(function(err) {
+        result.save(function (err) {
             res.send({
-                "status":"0",
-                "result":result
+                "status": "0",
+                "result": result
             });
         });
     });
 });
 
-router.get("/:id", function(req, res) {
+router.get("/:id", function (req, res) {
     var id = req.params.id;
     var userModel = sqlDBUtils.getModels().user;
-    userModel.find({id: id}, (err, result) => {
+    userModel.find({ id: id }, (err, result) => {
         if (err) throw err;
         res.send(result);
     });
 });
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     var id = req.params.id;
     var userModel = sqlDBUtils.getModels().user;
     userModel.find({}, (err, result) => {
